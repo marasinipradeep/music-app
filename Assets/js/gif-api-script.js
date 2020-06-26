f1efddd96eb16e7531bd976a0fc2f3b73cf2c6e0
miao0007 <miao188@hotmail.com>
2020-06-26 19:17:27 +0930

    find out Tenor gif API which is more accurate

--- Assets/js/gif-api-script.js
+++ Assets/js/gif-api-script.js
@@ -1,13 +1,28 @@
-var musicStyle = ["music guitar","classic music","metal music","piano music"];
-var APIKey = "iVX91FyysEevZSL0k2Fuo7MUT7i9ntZE";
-queryURL = "https://api.giphy.com/v1/gifs/search?q="+ musicStyle[Math.floor(Math.random() * 4)] +"&api_key="+APIKey+"&limit=1" ;
+var musicStyle = [
+  "music-guitar",
+  "classic-music",
+  "music-metal",
+  "piano-music",
+];
+// Tenor gif api
+var APIKey = "ZFKX4SZGA5FO";
+queryURL =
+  "https://api.tenor.com/v1/search?q=" +
+  musicStyle[Math.floor(Math.random() * 4)] +
+  "&key=ZFKX4SZGA5FO&limit=1";
+
+  // This is Giphy gif api, not that accurate, could be spare api
+  // var APIKey = "iVX91FyysEevZSL0k2Fuo7MUT7i9ntZE";
+// queryURL = "https://api.giphy.com/v1/gifs/search?q="+ musicStyle[Math.floor(Math.random() * 4)] +"&api_key="+APIKey+"&limit=1" ;
 console.log(queryURL);
- $.ajax({
-   url:queryURL,
-   method:"GET",
-   dataType:"json"
- }).then(function(response){
-var gif = $("<img>");
-gif.attr("src", response.data[0].images.original.url);
-$("#gif-wrap").append(gif);
- })
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+  dataType: "json",
+}).then(function (response) {
+  var gif = $("<img>");
+  gif.attr("src", response.results[0].media[0].gif.url);
+  // gif.attr("src", response.data[0].images.original.url);
+  $("#gif-wrap").append(gif);
+});
