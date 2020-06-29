73da5f3973fbc8788ce23e3c035d9925f7aaf3b1
marasinipradeep <marasinipradeep@gmail.com>
2020-06-29 14:17:55 +0930

    # Comments added refactored values on user choices

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,42 +1,54 @@
-//giphy
-//emVJkP2qAM1mr95yGqEgW0VXJHEfbTbO
+
 var counter = 0;
 init()
 function init() {
 }
 
+//Function displaying app title and current day on heading.
 function musicAppHeading() {
-    $("#header #appTitle").text("your choice of music...")
-    $("#header #appSubTitle").text(moment().format('dddd, MMMM Do'))
-
+    $("#header #appTitle").text("MUSIC-APP")
+    $("#header #currentDay").text(moment().format('dddd, MMMM Do'))
 }
 
-
+//Creating series of questions and options as an objects.
 var choiceOfCountry = {
-    Question: "Which country music do you like one ?",
-    Options: ["Australia", "America", "German", "India"]
+    Question: "Which country music do you like ?",
+   // Options: ["Australia", "America", "German", "India"]
+    Options: ["AUS", "USA", "UK", "IN"]
+   
+}
+
+var chiceOfGenre = {
+    Question: "Which Genre do you like ?",
+    //Options: ["Jazz", "Rock", "Classic", "Pop"],
+    Options: [0, 1, 2, 3]
+    
+}
+
+var choiceOfArtist = {
+    Question: "Which Artist do you like ?",
+   // Options: ["Australia", "America", "German", "India"],
+    Options: [0, 1, 2, 3]
+   
 }
 
 
-var typeOfSong = {
-    Question: "Which Type of music do you like two ?",
-    Options: ["Jazz", "Rock", "Classic", "Pop"]
+var choiceOfAlbum = {
+    Question: "Which Album do you like ?",
+   // Options: ["Australia", "America", "German", "India"],
+    Options: [0, 1, 2, 3]
+   
 }
 
-
-var choiceOfSinger = {
-    Question: "Which Type of music do you like three ?",
-    Options: ["Australia", "America", "German", "India"]
-}
-
-
+//Array of questions.All the questions of obejcts pushed inside the following array.
 var arrayOfQuestions = []
-arrayOfQuestions.push(choiceOfCountry, typeOfSong, choiceOfSinger)
-console.log(arrayOfQuestions)
+arrayOfQuestions.push(choiceOfCountry, chiceOfGenre, choiceOfArtist,choiceOfAlbum )
 
-var savedArrayOfChoices =[]
+//Array to hold different users choices.
+var savedArrayOfChoices = []
 
 
+//Function to give some css tricks for hiding and displaying html elements.
 function onStartButtonClicked() {
     $("#description").addClass("hide");
     $("#startButton").addClass("hide");
@@ -44,22 +56,14 @@ function onStartButtonClicked() {
     $("#header").addClass("show");
     $("img").addClass("hide")
     $("#initialPage").addClass("hide")
-
     $(".panel").removeClass("hide")
     $(".panel").addClass("show")
-
-    $(".flex-video").removeClass("hide")
-    $(".flex-video").addClass("show")
     musicAppHeading()
     showQuestion(counter)
 }
 
-
-
-
+//Functions to create series of questions.
 function showQuestion(counter) {
-
-    console.log("COunter is : " + counter)
     var newDiv = $("<div>").addClass("row")
     var newFieldSet = $("<fieldset>").addClass("small-6 columns newDiv")
     var newLegend = $("<legend>").text(arrayOfQuestions[counter].Question)
@@ -68,32 +72,35 @@ function showQuestion(counter) {
     showOption(counter)
 }
 
-
+//All the options related to questions are shown by following functions.
 function showOption(counter) {
 
     $.each(arrayOfQuestions[counter].Options, function (index, options) {
-        console.log(options)
-        // var newInput = $("<input>").attr({ type: "radio", value: "metal" })
-        // var newLabel = $("<label>").attr("for", "metalType").text(options)
-        var newButton = $("<button>").text(options).addClass("button success optionsButton").attr({type:"button",id:""}).data("value", options)
+        var newButton = $("<button>").text(options).addClass("button success optionsButton").attr({ type: "button", id: "" }).data("value", options)
         $(".newDiv").append(newButton)
-
-        newButton.on("click", function (event){
+        newButton.on("click", function (event) {
             event.preventDefault();
             var value = $(this).data("value")
             console.log("on button clicked : " + value)
             savedArrayOfChoices.push(value)
             console.log(...savedArrayOfChoices)
-            if(counter===2){
-                alert("End of questions")
+            if (counter === 3) {
+                $(".panel").addClass("hide")
+                $(".flex-video").removeClass("hide")
+                $(".flex-video").addClass("show")
+                getCountry(savedArrayOfChoices)
             }
-            counter ++;
+            else{
+            counter++;
             $(".inputForm").text("")
             $(".inputForm").empty()
             showQuestion(counter)
+        }
 
         })
     })
 }
+
+//Event handler 
 $("#startButton").on("click", onStartButtonClicked)
 
